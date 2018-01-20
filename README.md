29bb39c0e0bb671dc21731b03795d1b75a1f342c
Seth Walton <sdwalton83@gmail.com>
2018-01-20 13:34:57 -0600

    initial structure on sdw branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# News-Wise
-News thats wise (Ku-Coding BootCamp Project 2)
--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-var authController = require('../controllers/authcontroller.js');
- 
-module.exports = function(app) {
- 
-    app.get('/signup', authController.signup);
-
-    app.get('/signin', authController.signin);
- 
-}
