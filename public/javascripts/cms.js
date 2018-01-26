9facf2b6fcf78098439069d41bdeaf98d82279ca
Kyle Becker <kmbecker@users.noreply.github.com>
2018-01-25 20:25:54 -0600

    updated files

--- public/javascripts/cms.js
+++ public/javascripts/cms.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
-  // Gets an optional query string from our url (i.e. ?post_id=23)
+
   var url = window.location.search;
   var postId;
-  // Sets a flag for whether or not we're updating a post to be false initially
+
   var updating = false;
 
-  // If we have this section in our url, we pull out the post id from the url
-  // In localhost:8080/cms?post_id=1, postId is 1
   if (url.indexOf("?post_id=") !== -1) {
     postId = url.split("=")[1];
     getPostData(postId);
   }
 
-  // Getting jQuery references to the post body, title, form, and category select
   var bodyInput = $("#body");
   var titleInput = $("#title");
   var cmsForm = $("#cms");
   var postCategorySelect = $("#category");
-  // Giving the postCategorySelect a default value
+
   postCategorySelect.val("Personal");
+  
   // Adding an event listener for when the form is submitted
   $(cmsForm).on("submit", function handleFormSubmit(event) {
     event.preventDefault();
+   
     // Wont submit the post if we are missing a body or a title
     if (!titleInput.val().trim() || !bodyInput.val().trim()) {
       return;
@@ -57,7 +56,7 @@ $(document).ready(function() {
   function getPostData(id) {
     $.get("/api/posts/" + id, function(data) {
       if (data) {
-        // If this post exists, prefill our cms forms with its data
+      
         titleInput.val(data.title);
         bodyInput.val(data.body);
         postCategorySelect.val(data.category);
